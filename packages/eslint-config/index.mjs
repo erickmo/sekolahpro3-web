d11c7273f656f57d7bc13a8fc03e3a8ee5c1d843
Erick Mo <mo@thunderlab.id>
2026-05-23 16:59:38 +0700

    chore: shared tsconfig + eslint-config packages

---
+++ packages/eslint-config/index.mjs
@@ -0,0 +1,20 @@
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import reactPlugin from "eslint-plugin-react";
+import hooksPlugin from "eslint-plugin-react-hooks";
+
+export default [
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: { parser: tsParser, parserOptions: { ecmaVersion: 2022, sourceType: "module" } },
+    plugins: { "@typescript-eslint": tsPlugin, react: reactPlugin, "react-hooks": hooksPlugin },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...hooksPlugin.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+    settings: { react: { version: "detect" } },
+  },
+];
